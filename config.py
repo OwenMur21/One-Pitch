a283f4dcff622cb98efa96dc281dbac4bbf90486
Owenmur21 <owenmuriithi@gmail.com>
2018-09-08 00:57:03 +0300

    Adds email configurations

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ozzy:ozzie21@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
  
 class ProdConfig(Config):
     pass
