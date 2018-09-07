02f6b322781fd94dd27df0df93b3e29152e0aabb
Owenmur21 <owenmuriithi@gmail.com>
2018-09-07 11:39:33 +0300

    Creates route for pitching new ideas

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -10,3 +11,11 @@ def index():
     return render_template('index.html', title = title)
 
 
+@main.route('/pitches/new', methods = ['GET','POST'])
+@login_required
+def new_pitch(id):
+    """
+    View new pitch function that returns new pitches and its data
+    """
+
+    
