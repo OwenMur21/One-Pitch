a87e05b233bb8fdd25f7bb4b9c9427cc20705eec
Owenmur21 <owenmuriithi@gmail.com>
2018-09-07 13:02:08 +0300

    Shows relationships between classes

--- app/models.py
+++ app/models.py
@@ -10,6 +10,10 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
 
     @property
     def password(self):
@@ -32,6 +36,9 @@ class Pitch(db.Model):
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     category = db.Column(db.String(255))
     content = db.Column(db.String(255))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
+    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
 
     def __repr__(self):
         return f'The pitch category {self.category}'
@@ -43,6 +50,7 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     description = db.Column(db.String(255))
+    
 
     def __repr__(self):
         return f'Comment: {self.description}'
